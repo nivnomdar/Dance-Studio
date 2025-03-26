d1dabc4d969464e3fa02fa8fa7e107eaa0c3680e
niv nomdar <nivnomdar1@gmail.com>
2025-03-26 21:58:14 +0200

    Swiper

--- frontend/src/components/PhotosCarousel.tsx
+++ frontend/src/components/PhotosCarousel.tsx
@@ -1,9 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import { Pagination, Autoplay, Scrollbar } from "swiper/modules";
 
-import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/swiper-bundle.css";
 
 function PhotosCarousel() {
   return (
@@ -13,9 +11,8 @@ function PhotosCarousel() {
       autoplay={{ delay: 3000 }}
       loop={true}
       pagination={{ clickable: true }}
-      modules={[Pagination, Autoplay]}
-      scrollbar={{ draggable: true }}
-      // modules={[Scrollbar]}
+      modules={[Pagination, Autoplay, Scrollbar]}
+      // scrollbar={{ draggable: true }}
     >
       <SwiperSlide>
         <img src="/carousel/image1.png" alt="Image 1" />
@@ -30,29 +27,6 @@ function PhotosCarousel() {
         <img src="/carousel/image4.png" alt="Image 4" />
       </SwiperSlide>
     </Swiper>
-
-    // <div>
-    //   <Carousel
-    //     // width={400}
-    //     showThumbs={true}
-    //     // infiniteLoop={true}
-    //     autoPlay={true}
-    //     // interval={4000}
-    //   >
-    //     <div key="1">
-    //       <img src="/carousel/image1.png" alt="Image 1" />
-    //     </div>
-    //     <div key="2">
-    //       <img src="/carousel/image2.png" alt="Image 2" />
-    //     </div>
-    //     <div key="3">
-    //       <img src="/carousel/image3.png" alt="Image 3" />
-    //     </div>
-    //     <div key="4">
-    //       <img src="/carousel/image4.png" alt="Image 4" />
-    //     </div>
-    //   </Carousel>
-    // </div>
   );
 }
 
